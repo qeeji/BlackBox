54803d9deacbd7e9013ca676d14f3a6b7470bc88
qeeji <qeejihz@gmail.com>
2018-11-23 23:31:48 +0800

    Update time_change_signal.py
    add new tip for UI parent

--- time_change_signal.py
+++ time_change_signal.py
@@ -13,3 +13,34 @@ time_event_id = OpenMaya.MTimerMessage.addTimerCallback (3, func)
 
 #- delete it, don't run this with up codes..
 OpenMaya.MMessage.removeCallback(time_event_id)
+
+
+
+
+##################### try to add this call back to UI parent 
+import maya.cmds as cmd
+import maya.OpenMaya as om
+import maya.OpenMayaUI as mui
+
+from functools import partial
+
+def func(input , *args):
+    print (input +'aaa')
+    
+    
+time_event_id = om.MTimerMessage.addTimerCallback(1 , partial(func,'bbb ') )
+
+
+#om.MMessage.removeCallback(time_event_id)
+
+def removeCallback(inID , *args):
+    om.MMessage.removeCallback(inID)
+    
+
+win = cmd.window()
+cmd.columnLayout()
+cmd.button()
+cmd.showWindow(win)
+
+#aa = om.MString(win)
+uiid = mui.MUiMessage.addUiDeletedCallback(win , partial(removeCallback ,time_event_id ))
