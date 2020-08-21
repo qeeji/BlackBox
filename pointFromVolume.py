be36262974f0b4e61cca9b45fd02a6c872a95ed1
qeeji <qeejihz@gmail.com>
2020-08-21 10:40:45 +0800

    Update pointFromVolume.py

--- pointFromVolume.py
+++ pointFromVolume.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
 
 """
-Get the position information of the points that fill the inside of the selected mesh or are scattered on the surface.
-Reproduced the [PointFromVolume] and [scatter] nodes of Houdini.
+選択したメッシュ内部を埋め尽くす、または表面に散布されるポイントのポジション情報を取得。
+Houdini の 【 PointFromVolume 】,【 scatter 】 ノードを再現。
 """
 
-import  random
+import random
 import maya.cmds as cmds
 import maya.api.OpenMaya as om
 
@@ -13,24 +13,24 @@ import maya.api.OpenMaya as om
 class PointFromVolume(object):
     def __init__(self):
         """
-        Store the selected mesh data in meshFn
+        meshFn に選択したメッシュデータを格納
         """
 
-        self . sel  =  om . MGlobal . getActiveSelectionList ()
-        self . meshFn  =  om . MFnMesh ( self . Cell . GetDagPath ( 0 ))
+        self.sel = om.MGlobal.getActiveSelectionList()
+        self.meshFn = om.MFnMesh(self.sel.getDagPath(0))
 
     def getBoundingBox(self):
         """
-        Get bounding information for selected objects
+        選択したオブジェクトのバウンディング情報を取得
         """
 
         self.boundingBox = om.MBoundingBox()
-        self . pointArray  =  om . MPointArray ()
+        self.pointArray = om.MPointArray()
 
-        #Get location information of point
+        # ポイントの位置情報を取得
         self.pointArray = self.meshFn.getPoints(om.MSpace.kWorld)
 
-        for  points  in  range ( len ( self . pointArray )):
+        for points in range(len(self.pointArray)):
             self.point = self.pointArray[points]
             self.boundingBox.expand(self.point)
 
@@ -38,94 +38,94 @@ class PointFromVolume(object):
 
     def setPointFromVolume(self, distance, surface=False, rand=False):
         """
-        Scatter points to mesh and get position information for that point
-        argument:
-            distance(float): Distance between points. The smaller the value, the denser the density
-            surface(bool): If True, scatter on the surface of the mesh
-            rand(float): Placed in random. Numerical value becomes random strength value as it is
+        mesh にポイントを散布し、そのポイントのポジション情報を取得
+        引数:
+            distance(float) : ポイント間の距離。値が小さいほど密度が濃い
+            surface(bool)   : True の場合、メッシュの表面に散布
+            rand(float)     : random に配置。数値がそのままランダム強度値となる
         """
 
-        # Calculate the offset applied to the minimum and maximum corners of the bounding box
-        self . halfVoxelDist  =  0.5  *  distance
-        # Get the minimum bounding box
+        # 境界ボックスの最小角と最大角に適用するオフセットを計算
+        self.halfVoxelDist = 0.5 * distance
+        # バウンディングボックスの最小値を取得
         self.minPoint = self.getBoundingBox().min
         self.minPoint.x += self.halfVoxelDist
         self.minPoint.y += self.halfVoxelDist
         self.minPoint.z += self.halfVoxelDist
-        #Get maximum value of bounding box
+        # バウンディングボックスの最大値を取得
         self.maxPoint = self.getBoundingBox().max
         self.maxPoint.x += self.halfVoxelDist
-        self . maxPoint . y  + =  self . halfVoxelDist
+        self.maxPoint.y += self.halfVoxelDist
         self.maxPoint.z += self.halfVoxelDist
 
         def floatIterator(start, stop, step):
             """
-            Stores the calculated value by repeating the number within the range from start to stop for the number specified in step.
-            example)
-                Get the equal number of position information of the specified number (step) between the minimum value (start) and the maximum value (stop) of the bounding box.
+            start ~ stop 範囲内の数値で step で指定した数分繰り返し、計算値を格納
+            例)
+                バウンディングボックスの最小値(start) ~ 最大値(stop) の間で指定した数(step) の均等なポジション情報を取得。
             """
             r = start
             while r < stop:
                 yield r
                 r += step
 
-        #Declare position information after calculation
+        # 計算後のポジション情報を宣言
         self.voxels = []
         for xCoord in floatIterator(self.minPoint.x, self.maxPoint.x, distance):
             for yCoord in floatIterator(self.minPoint.y, self.maxPoint.y, distance):
                 for zCoord in floatIterator(self.minPoint.z, self.maxPoint.z, distance):
 
-                    #Cross judgment parameters
-                    self . raySource  =  om . MFloatPoint ( xCoord , yCoord , zCoord )
+                    # 交差判定のパラメータ
+                    self.raySource = om.MFloatPoint(xCoord, yCoord, zCoord)
                     self.maxParam = 9999.0
                     self.tolerance = 0.0001
                     randX = random.uniform(-1, 1) * rand
                     randY = random.uniform(-1, 1) * rand
                     randZ = random.uniform(-1, 1) * rand
 
-                    # Branch when setting arguments
+                    # 引数設定時の分岐
                     if surface == True:
                         if rand != False:
-                            self . raySource  + =  om . MFloatPoint (
+                            self.raySource += om.MFloatPoint(
                                 randX, randY, randZ)
-                            self . rayDirection  =  on . MFloatVector (
+                            self.rayDirection = om.MFloatVector(
                                 randX, randY, randZ)
-                        elif  rand  ==  False :
-                            rand  =  1
-                            self . raySource  + =  om . MFloatPoint (
+                        elif rand == False:
+                            rand = 1
+                            self.raySource += om.MFloatPoint(
                                 randX, randY, randZ)
-                            self . rayDirection  =  on . MFloatVector (
+                            self.rayDirection = om.MFloatVector(
                                 randX, randY, randZ)
                     elif surface == False:
                         if rand != False:
-                            self . raySource  + =  om . MFloatPoint (
+                            self.raySource += om.MFloatPoint(
                                 randX, randY, randZ)
-                            self . rayDirection  =  on . MFloatVector (
+                            self.rayDirection = om.MFloatVector(
                                 randX, randY, randZ)
-                        elif  rand  ==  False :
-                            self . rayDirection  =  on . MFloatVector ( 0 , 0 , - 1 )
+                        elif rand == False:
+                            self.rayDirection = om.MFloatVector(0, 0, -1)
 
-                    #Cross judgment
-                    self . ret  =  self . meshFn . allIntersections (
-                        Self . RaySource ,              # RaySource ---------- Rei start point
-                        Self . RayDirection ,           the direction of the # rayDirection ------- Rei
-                        Om . MSpace . KWorld ,            coordinate space # coordinate space --- hit point is specified
-                        Self . MaxParam ,               maximum radius consider the # maxParam ----------- hit
-                        False ,                       # testBothDirections-Whether to consider negative rayDirection hits
-                        tolerance = self . tolerance ,    # tolerance ---------- numerical tolerances intersection operation
+                    # 交差判定
+                    self.ret = self.meshFn.allIntersections(
+                        self.raySource,             # raySource ---------- レイスタートポイント
+                        self.rayDirection,          # rayDirection ------- レイの方向
+                        om.MSpace.kWorld,           # coordinate space --- ヒットポイントが指定されている座標空間
+                        self.maxParam,              # maxParam ----------- ヒットを考慮する最大半径
+                        False,                      # testBothDirections - 負のrayDirectionのヒットも考慮する必要があるかどうか
+                        tolerance=self.tolerance,   # tolerance ---------- 交差操作の数値許容差
                     )
 
-                    Branch of the return value when the # argument is set
+                    # 引数設定時の戻り値の分岐
                     if surface == True:
                         if (len(self.ret[0]) % 2 == 1):
                             self.voxels.append(
-                                ( self . ret [ 0 ] [ 0 ] [ 0 ], self . ret [ 0 ] [ 0 ] [ 1 ], self . ret [ 0 ] [ 0 ] [ 2 ]))
-                        elif  len ( self . ret [ 0 ]) %  2  ==  0  and  len ( self . ret [ 0 ]) ! =  0 :
+                                (self.ret[0][0][0], self.ret[0][0][1], self.ret[0][0][2]))
+                        elif len(self.ret[0]) % 2 == 0 and len(self.ret[0]) != 0:
                             self.voxels.append(
-                                ( self . ret [ 0 ] [ 0 ] [ 0 ], self . ret [ 0 ] [ 0 ] [ 1 ], self . ret [ 0 ] [ 0 ] [ 2 ]))
+                                (self.ret[0][0][0], self.ret[0][0][1], self.ret[0][0][2]))
 
                     elif surface == False:
-                        #If the skipped ray hits only once, it is judged to be inside the mesh, otherwise it is judged to be outside the mesh
+                        # 飛ばしたレイが1回のみ当たった場合はメッシュの中、それ以外はメッシュの外と判定
                         if (len(self.ret[0]) % 2 == 1):
                             self.voxels.append(self.raySource)
 
@@ -133,10 +133,10 @@ class PointFromVolume(object):
 
 
 
-#Sample script
+# サンプルスクリプト
 """
 distance = 3
-cls = PointFromVolume ()
+cls = PointFromVolume()
 pos = cls.setPointFromVolume(distance, surface=False, rand=0)
 grp = []
 for i in pos:
