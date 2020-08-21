ae2ef5035d56dcd1ddf3d09e984371fb4ceee1b7
qeeji <qeejihz@gmail.com>
2020-08-21 10:26:38 +0800

    Update getPointAtUV.py

--- getPointAtUV.py
+++ getPointAtUV.py
@@ -3,34 +3,32 @@ import maya.cmds as cmds
 import random
 
 def GetDagPath(nodeName):
-sel = om.MSelectionList()
-om.MGlobal.getSelectionListByName(nodeName, sel)
+  sel = om.MSelectionList()
+  om.MGlobal.getSelectionListByName(nodeName, sel)
 
-dp = om.MDagPath()
+  dp = om.MDagPath()
 
-sel.getDagPath(0,dp)
-return dp
+  sel.getDagPath(0,dp)
+  return dp
 
 
 def UvCoordToWorld(U, V, mesh):
+  mfnMesh = om.MFnMesh(GetDagPath(mesh))
+  numFaces = mfnMesh.numPolygons()
+  WSpoint = om.MPoint(0.0,0.0,0.0)
 
-mfnMesh = om.MFnMesh(GetDagPath(mesh))
-numFaces = mfnMesh.numPolygons()
+  util2 = om.MScriptUtil()
+  util2.createFromList ((U, V),2)
+  float2ParamUV = util2.asFloat2Ptr()
 
-WSpoint = om.MPoint(0.0,0.0,0.0)
+  for i in range(numFaces):
+  try:
+  mfnMesh.getPointAtUV(i,WSpoint, float2ParamUV, om.MSpace.kWorld)
+  break; #point is in poly
+  except:
+  continue #point not found!
 
-util2 = om.MScriptUtil()
-util2.createFromList ((U, V),2)
-float2ParamUV = util2.asFloat2Ptr()
-
-for i in range(numFaces):
-try:
-mfnMesh.getPointAtUV(i,WSpoint, float2ParamUV, om.MSpace.kWorld)
-break; #point is in poly
-except:
-continue #point not found!
-
-return WSpoint
+  return WSpoint
 
 f = UvCoordToWorld (random.random(), random.random(), cmds.ls(sl=True,type='shape'))
 print [f[0], f[1], f[2]]
